eab6a3611694dd6bf7cc350c24f7f1ea2137f1cb
Ahmet Buğra Tohumcu <tohumcuahmetbugra@gmail.com>
2025-04-30 17:32:49 +0300

    AuthService Created

---
+++ Backend/full-stack-login-app/src/main/java/com/bugra/full_stack_login_app/service/AuthService.java
@@ -0,0 +1,34 @@
+package com.bugra.full_stack_login_app.service;
+
+import com.bugra.full_stack_login_app.model.User;
+import com.bugra.full_stack_login_app.request.UsernamePasswordRequest;
+import com.bugra.full_stack_login_app.security.JWTokenProvider;
+import com.bugra.full_stack_login_app.service.user_services.UserService;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+    private final UserService userService;
+    private final JWTokenProvider tokenProvider;
+
+    public AuthService(UserService userService, JWTokenProvider tokenProvider) {
+        this.userService = userService;
+        this.tokenProvider = tokenProvider;
+    }
+
+    public String loginAndGenerateToken(UsernamePasswordRequest request) {
+        String token = null;
+        User user = userService.getByUserName(request.getUsername());
+
+        if(user != null){
+            UsernamePasswordAuthenticationToken authToken =
+                    new UsernamePasswordAuthenticationToken(user.getUsername(),user.getPassword());
+            token = tokenProvider.generateToken(user.getUsername());
+            return token;
+        }
+
+        return null;
+    }
+}
