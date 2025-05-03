eb02112ffe19a5d239f8076ac06a9a97536e620c
Ahmet Buğra Tohumcu <tohumcuahmetbugra@gmail.com>
2025-05-03 18:43:41 +0300

    method updated

--- Backend/full-stack-login-app/src/main/java/com/bugra/full_stack_login_app/controller/AuthController.java
+++ Backend/full-stack-login-app/src/main/java/com/bugra/full_stack_login_app/controller/AuthController.java
@@ -8,6 +8,8 @@ import com.bugra.full_stack_login_app.service.AuthService;
 import com.bugra.full_stack_login_app.service.CookieService;
 import com.bugra.full_stack_login_app.service.user_services.UserService;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 
 @RestController
@@ -25,6 +29,7 @@ public class AuthController {
     private final UserService userService;
     private final AuthService authService;
     private final CookieService cookieService;
+    private final Logger logger = LoggerFactory.getLogger(AuthController.class);
 
     public AuthController(UserService userService,AuthService authService, CookieService cookieService) {
         this.userService = userService;
@@ -43,7 +48,7 @@ public class AuthController {
     public ResponseEntity<String> login(@RequestBody UsernamePasswordRequest request , HttpServletResponse response){
         try{
             String token = authService.loginAndGenerateToken(request);
-            System.out.println("JWT Token : "+ token);
+            logger.debug("Extracted JWT token:  {}" ,token);
             ResponseCookie responseCookie = cookieService.createResponseCookie("JWT", token);
             response.addHeader("Set-Cookie" , responseCookie.toString());
             return new ResponseEntity<>(UserResponseMessage.LOGIN_SUCCESSFUL ,HttpStatus.ACCEPTED);
