eca731fed743c8c5f73041386c078f952990bb1e
Ahmet Buğra Tohumcu <tohumcuahmetbugra@gmail.com>
2025-05-08 17:49:23 +0300

    method updated

--- Frontend/auth-system/src/FormHandlers/LoginHandler.js
+++ Frontend/auth-system/src/FormHandlers/LoginHandler.js
@@ -4,12 +4,15 @@ export async function submitLoginForm() {
     let username = document.getElementById("user-name").value;
     let password = document.getElementById("password").value;
     let response = document.getElementById("username-response");
-    let res = await submitForm('http://localhost:8080/login', username, password, response);
-
-    if(res && res.ok) {
-        window.location.href = 'http://localhost:8080/hello';
-    }else{
-        alert("Login failed! Please check your credentials.");
-    }
-
+    await submitForm('http://localhost:8080/login', username, password, response);
+    
+    return fetch('http://localhost:8080/hello', {
+            method: 'GET',
+            headers: { 'Content-Type': 'text/plain' },
+            credentials: 'include'
+        }).then(()=>{
+            window.location.href = 'http://localhost:8080/hello';
+        }).catch(err => {
+            response.innerHTML = `${err.message}`
+        });
 }
